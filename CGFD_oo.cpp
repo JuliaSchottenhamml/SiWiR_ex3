1152ca26b66f2235d859232655adff37f12901da
karnajitsen <karnajitsen@gmail.com>
2014-12-28 21:33:01 +0100

    upd

--- CGFD_oo.cpp
+++ CGFD_oo.cpp
@@ -41,23 +41,50 @@ inline double compute2norm(std::vector<double> vec)
 
 }
 
-inline double * matMult( std::vector<double> vec,GridCaptain gcap,const double alpha, const double beta, const double gama,
- const int * dim ,int size, int rank, int tgrdpoint)
-{  
+inline std::vector<double> matMult(FdGrid& fgrid, std::vector<double> vec,/*GridCaptain& gcap,*/const double alpha, const double beta, const double gama)
+{
+
+   int size(0); // The total number of processes
+   int rank(0); // The rank/number of this process (within MPI_COMM_WORLD)
+  // int proc = gcap.proc;
    int veclen = (int)vec.size();
    vector<double> fresult(veclen,0);
    
    std::cout << "1 " << "\n";
    
-   
+   // Initialization of MPI
+   // ----------------------------------------------------------------
+   MPI_Init(NULL,NULL);
+   // ----------------------------------------------------------------
+
+   // Determining the number of CPUs and the rank of this process
+   // ----------------------------------------------------------------
+   MPI_Comm_size( MPI_COMM_WORLD, &size );
+   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+   // ----------------------------------------------------------------   
+ std::cout << "2 " << "\n";
+   //int MPI_Cart_shift(MPI_COMM_WORLD,0,1,rank,rank+2);
     int *rec_cnt = new int[size];
    int *rec_disp = new int[size];
+   
+    int * dim = new int [2];
+    GridCaptain* gcap = NULL;
+   if (rank == 0)
+   {  
+    gcap = new GridCaptain(size,fgrid);
+    dim[0]=fgrid.getDimM();
+    dim[1]=fgrid.getDimN();
+   
+    MPI_Bcast(dim,1,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(gcap,1,MPI_INT,0,MPI_COMM_WORLD);
+     std::cout << "3 " << "\n";
+   }
     int bleny =  dim[1];  
    //  int proc = size;  
     
-    int blenx = gcap.worksheet[rank*3+1];
-    int sx = gcap.worksheet[rank*3+0];
-    rec_disp[rank] =  gcap.worksheet[rank*3+2];
+    int blenx = gcap->worksheet[rank*3+1];
+    int sx = gcap->worksheet[rank*3+0];
+    rec_disp[rank] =  gcap->worksheet[rank*3+2];
     int sy = 0;
     //int ex = sx+blenx-1;
    // int ey = bleny;
@@ -65,13 +92,19 @@ inline double * matMult( std::vector<double> vec,GridCaptain gcap,const double a
     double * result = new double[sz];
     int gridno = 0;
 
-
+//    double *ss = new double[blenx];
+  //  double *ns = new double[blenx];
             double gama1 = 0;
             double gama2 = 0;
             double beta1 = 0;
             double beta2 = 0;
 
-
+   
+    //double *sr = new double[blenx];
+    //double *nr = new double[blenx];
+                 
+    //ss=fgrid.getDataAdd(sx,sy);
+    //ns=fgrid.getDataAdd(ex,sy); 
     int rank1 =  rank+1;  
     int rank2 =  rank-1; 
 
@@ -122,41 +155,92 @@ inline double * matMult( std::vector<double> vec,GridCaptain gcap,const double a
     }
 
     MPI_Allgatherv(result,sz, MPI_DOUBLE, (void*)&fresult, rec_cnt,rec_disp, MPI_DOUBLE,MPI_COMM_WORLD );
-   // MPI_Finalize();
+    MPI_Finalize();
     
-    if(rank == 0)
     return fresult;
-    else
-    return NULL;
 }
 
 
-inline std::vector<double> cal_fVec(GridCaptain gcap, const double gama, const int * dim, int size, int rank, int tgrdpoint)
-{ 
-   vector<double> fresult(tgrdpoint,0);
+inline std::vector<double> cal_fVec(FdGrid& fgrid,/*GridCaptain& gcap,*/ double gama)
+{
 
+   int size(0); // The total number of processes
+   int rank(0); // The rank/number of this process (within MPI_COMM_WORLD)
+  // int proc = gcap.proc;
+   //int veclen = (int)vec.size();
+   vector<double> fresult(fgrid.totalGridPoints(),0);
+  
+   
+   // Initialization of MPI
+   // ----------------------------------------------------------------
+   MPI_Init(NULL,NULL);
+   // ----------------------------------------------------------------
+
+   // Determining the number of CPUs and the rank of this process
+   // ----------------------------------------------------------------
+   MPI_Comm_size( MPI_COMM_WORLD, &size );
+   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+   // ----------------------------------------------------------------   
+   
+
+   //int MPI_Cart_shift(MPI_COMM_WORLD,0,1,rank,rank+2);
+   GridCaptain* gcap = NULL;
+    int * dim = new int [2];
+   if (rank == 0)
+   {  
+
+    gcap = new GridCaptain(size,fgrid);
+    dim[0]=fgrid.getDimM();
+    dim[1]=fgrid.getDimN();
+   
+    MPI_Bcast(dim,1,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(gcap,1,MPI_INT,0,MPI_COMM_WORLD);
+    
+   }
+   
     int *rec_cnt = new int[size];
    int *rec_disp = new int[size];
-
+   
+     //   int proc = size;
+       //int eval=0, wval = 0, sval = 0, nval = 0, cval = 0;
     int bleny =  dim[1];    
     
-    int blenx = gcap.worksheet[rank*3+1];
-    int sx = gcap.worksheet[rank*3+0];
-    rec_disp[rank] =  gcap.worksheet[rank*3+2];
+    int blenx = gcap->worksheet[rank*3+1];
+    int sx = gcap->worksheet[rank*3+0];
+    rec_disp[rank] =  gcap->worksheet[rank*3+2];
     int sy = 0;
+    //int ex = sx+blenx-1;
+    //int ey = bleny;
     int sz=blenx*bleny;
     double * result = new double[sz];
     int gridno = 0;
     double hx = fgrid.getHx();
     double hy = fgrid.getHy();
 
+    //double *ss = new double[blenx];
+    //double *ns = new double[blenx];
+    
+    //double *sr = new double[blenx];
+    //double *nr = new double[blenx];
+                 
 
+  //  ss=fgrid.getDataAdd(sx,sy);
+    //ns=fgrid.getDataAdd(ex,sy);
+    //int rank1 =  rank+1;  
     int rank2 =  rank-1; 
 
+   // int rn = MPI_Cart_shift(MPI_COMM_WORLD,0,1,&rank,&rank1 );
     int rs = MPI_Cart_shift(MPI_COMM_WORLD,0,-1,&rank,&rank2);
-   
-     double gama2 = 0;
 
+    /* MPI_ISend(ss,1,MPI_double,rs, MPI_COMM_WORLD);
+     MPI_ISend(ns,1,MPI_double,rn, MPI_COMM_WORLD);
+
+     MPI_recv(sr,1,MPI_double,rs, MPI_COMM_WORLD);
+     MPI_recv(nr,1,MPI_double,rn, MPI_COMM_WORLD);*/
+     //double gama1 = 0;
+     double gama2 = 0;
+     //double beta1 = 0;
+     //double beta2 = 0;
     int x = 0;
     int y = 0;
 
@@ -183,23 +267,124 @@ inline std::vector<double> cal_fVec(GridCaptain gcap, const double gama, const i
     }
 
     MPI_Allgatherv(result,sz, MPI_DOUBLE,  (void*)&fresult, rec_cnt,rec_disp, MPI_DOUBLE,MPI_COMM_WORLD );
-    //MPI_Finalize();
-    if(rank == 0)
-    return fresult;
-    else
-    return NULL;
+    MPI_Finalize();
+    
+    return fresult; 
+}
+
+
+inline std::vector<double> callCG(FdGrid& fgrid, int const iter, int const err)
+{
+    int len = 0;
+
+    if(fgrid.totalGridPoints()%4 != 0)
+    len = fgrid.totalGridPoints() + (4-fgrid.totalGridPoints());
+    std::vector<double> Xvec (len,0);
+    std::vector<double> Rvec (len,0);
+    std::vector<double> Fvec (len,0);
+    std::vector<double> Tvec (len,0);
+    std::vector<double> Tmpvec (len,0);
+    double alpha = 0;
+    double hx = fgrid.getHx();
+    double hy = fgrid.getHy();
+    
+    double alfa=0;
+        double bita=0;
+            double gama=0;
+            
+            bita = 1/hx/hx;
+            gama = 1/hy/hy;
+            alfa = -(2/gama+ 2/bita + k * k);
+
+    //GridCaptain* gcap = new GridCaptain(proc,fgrid);
+    
+    Tvec = matMult(fgrid,Xvec,alfa, bita, gama);
+    
+    Fvec = cal_fVec(fgrid,gama);
+
+    std::transform (Fvec.begin(), Fvec.end(), Tvec.begin(), Rvec.begin(),  std::minus<double>());
+
+    double dt0 = std::inner_product(Rvec.begin(), Rvec.end(), Rvec.begin(),0);
+
+    double resd = compute2norm(Rvec);
+
+    if(resd < err)
+        return Xvec;
+
+      std::vector<double> Dvec (Rvec);
+
+    for(int i = 0 ; i<iter; i++)
+
+    {
+        Tvec = matMult(fgrid,Dvec, alfa, bita, gama);
+
+        double dt = std::inner_product(Dvec.begin(), Dvec.end(), Tvec.begin(),0);
+
+        alpha = dt0 / dt;
+
+        //std::transform (Dvec.begin(), Dvec.end(), Tmpvec.begin(),  std::multiplies<double>(),alpha);
+        
+        //__m256d a,b,c;
+        
+        for(int j=0; j< (int)Dvec.size();j+=4)
+        {
+            Tmpvec[j] = alpha * Dvec[j];
+             Tmpvec[j+1] = alpha * Dvec[j+1];
+              Tmpvec[j+2] = alpha * Dvec[j+2];
+               Tmpvec[j+3] = alpha * Dvec[j+3]; 
+            
+        }      
+        
+
+        std::transform (Xvec.begin(), Xvec.end(), Tmpvec.begin(), Tvec.begin(),   std::plus<double>());
+
+        //std::transform (Tvec.begin(), Tvec.end(), Tmpvec.begin(),  std::multiplies<double>(),alpha);
+        
+        for(int j=0; j< (int)Tvec.size();j+=4)
+        {
+           Tmpvec[j] = alpha * Tvec[j];
+             Tmpvec[j+1] = alpha * Tvec[j+1];
+              Tmpvec[j+2] = alpha * Tvec[j+2];
+               Tmpvec[j+3] = alpha * Tvec[j+3]; 
+            
+        }      
+        
+        std::transform (Rvec.begin(), Rvec.end(), Tmpvec.begin(), Rvec.begin(),  std::minus<double>());
+
+        double dt1 = std::inner_product(Rvec.begin(), Rvec.end(), Rvec.begin(),0);
+
+        resd = compute2norm(Rvec);
+
+        if(resd < err)
+            return Xvec;
+
+        double beta = dt1/dt0;
+
+        //std::transform (Dvec.begin(), Dvec.end(), Tmpvec.begin(),  std::multiplies<double>(),beta);
+        
+         for(int j=0; j< (int)Dvec.size();j+=4)
+        {
+             Tmpvec[j] = beta * Dvec[j];
+             Tmpvec[j+1] = beta * Dvec[j+1];
+              Tmpvec[j+2] = beta * Dvec[j+2];
+               Tmpvec[j+3] = beta * Dvec[j+3]; 
+        }      
+        
+        std::transform (Rvec.begin(), Rvec.end(), Tmpvec.begin(), Tvec.begin(),   std::plus<double>());
+
+        dt0 = dt1;
+
+    }
+
+    return Xvec;
+
 }
 
 
 int main(int argc, char** argv)
 {
 
-    int size(0); // The total number of processes
-   int rank(0); // The rank/number of this process (within MPI_COMM_WORLD)
-  // int proc = gcap.proc;
-   //int veclen = (int)vec.size();
-   
-   int nx = 0;
+    int nx = 0;
     int ny = 0;
     int iter = 0;
     //int proc = 0;
@@ -238,8 +423,6 @@ int main(int argc, char** argv)
 	std::cout << "ny," << ny << std::endl;
 	std::cout << "c," << iter <<std::endl;
 	
-	int gridpoint = fgrid->totalGridPoints();
-   	
     ///******************************************************
 	///********************** CALCULATION *******************
 	///******************************************************
@@ -257,81 +440,56 @@ int main(int argc, char** argv)
         
     int len = 0;
 
-    if(gridpoint%4 != 0)
-    len = gridpoint + (4-gridpoint%4);
+    if(fgrid->totalGridPoints()%4 != 0)
+    len = fgrid->totalGridPoints() + (4-fgrid->totalGridPoints());
     std::vector<double> Xvec (len,0);
     std::vector<double> Rvec (len,0);
     std::vector<double> Fvec (len,0);
     std::vector<double> Tvec (len,0);
     std::vector<double> Tmpvec (len,0);
-    int * dim = new int [2];         
-    GridCaptain* gcap = NULL;
-    double * result=NULL;
     double alpha = 0;
     double hx = fgrid->getHx();
     double hy = fgrid->getHy();
     
     double alfa=0;
-    double bita=0;
-    double gama=0;
+        double bita=0;
+            double gama=0;
+            
             
             bita = 1/hx/hx;
             gama = 1/hy/hy;
             alfa = -(2/gama+ 2/bita + k * k);
-            
- // Initialization of MPI
-   // ----------------------------------------------------------------
-   MPI_Init(&argc, &argv);
-   // ----------------------------------------------------------------
 
-   // Determining the number of CPUs and the rank of this process
-   // ----------------------------------------------------------------
-   MPI_Comm_size( MPI_COMM_WORLD, &size );
-   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-   // ----------------------------------------------------------------   
-
-   if (rank == 0)
-   {  
-    gcap = new GridCaptain(size,fgrid);
-    dim[0]=fgrid.getDimM();
-    dim[1]=fgrid.getDimN();   
-    MPI_Bcast(dim,1,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(gcap,1,MPI_INT,0,MPI_COMM_WORLD);
-    // std::cout << "3 " << "\n";
-   }
-     
     //GridCaptain* gcap = new GridCaptain(proc,fgrid);
     
-    Tvec = matMult(Xvec,*gcap, alfa, bita,gama, dim, size, rank,gridpoint);    
-       
-    Fvec = cal_fVec(*gcap,gama, size, rank, gridpoint);
+    Tvec = matMult(fgrid,Xvec,alfa, bita, gama);
     
-    if(rank==0)
-    {
+    Fvec = cal_fVec(fgrid,gama);
 
     std::transform (Fvec.begin(), Fvec.end(), Tvec.begin(), Rvec.begin(),  std::minus<double>());
 
     dt0 = std::inner_product(Rvec.begin(), Rvec.end(), Rvec.begin(),0);
 
     resd = compute2norm(Rvec);
-      std::vector<double> Dvec (Rvec);
-    }
 
-    if(resd > err)
-     {            
-        for(int i = 0 ; i<iter; i++)
-       {
-        
-        MPI_Bcast(Dvec,1,MPI_INT,0,MPI_COMM_WORLD);
-        
-        Tvec = matMult(Dvec,*gcap, alfa, bita, gama, size, rank, gridpoint);
-        
-        if(rank == 0)
-        {
+    if(resd < err)
+        return Xvec;
+
+      std::vector<double> Dvec (Rvec);
+
+    for(int i = 0 ; i<iter; i++)
+
+    {
+        Tvec = matMult(fgrid,Dvec, alfa, bita, gama);
+
         double dt = std::inner_product(Dvec.begin(), Dvec.end(), Tvec.begin(),0);
 
         alpha = dt0 / dt;
-              
+
+        //std::transform (Dvec.begin(), Dvec.end(), Tmpvec.begin(),  std::multiplies<double>(),alpha);
+        
+        //__m256d a,b,c;
+        
         for(int j=0; j< (int)Dvec.size();j+=4)
         {
             Tmpvec[j] = alpha * Dvec[j];
@@ -339,9 +497,12 @@ int main(int argc, char** argv)
               Tmpvec[j+2] = alpha * Dvec[j+2];
                Tmpvec[j+3] = alpha * Dvec[j+3]; 
             
-        }     
+        }      
+        
 
         std::transform (Xvec.begin(), Xvec.end(), Tmpvec.begin(), Tvec.begin(),   std::plus<double>());
+
+        //std::transform (Tvec.begin(), Tvec.end(), Tmpvec.begin(),  std::multiplies<double>(),alpha);
         
         for(int j=0; j< (int)Tvec.size();j+=4)
         {
@@ -376,12 +537,11 @@ int main(int argc, char** argv)
         std::transform (Rvec.begin(), Rvec.end(), Tmpvec.begin(), Tvec.begin(),   std::plus<double>());
 
         dt0 = dt1;
-        }
+
     }
 
-}   
     
-    MPI_Finalize();
+    
     	time = timer.elapsed();
 	std::cout << "time," << time << std::endl;
 
