b94829a5bf4abb15a334a9fa929e420733456857
karnajitsen <karnajitsen@gmail.com>
2014-12-28 23:50:29 +0100

    upd

--- CGFD.cpp
+++ CGFD.cpp
@@ -72,7 +72,7 @@ std::cout << "3 " << "\n";
             double beta2 = 0;
 
 
-    int destn, dests;  
+    int destn = 0, dests = 0;  
     
 std::cout << "2### " << "\n";
     //MPI_Cart_shift(MPI_COMM_WORLD,0,1,&destn,&dests );
