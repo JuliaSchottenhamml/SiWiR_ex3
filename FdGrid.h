3edac794817b8c7051a9e3d5d36c1e027992cff4
karnajitsen <karnajitsen@gmail.com>
2015-01-03 18:01:28 +0100

    upd

--- FdGrid.h
+++ FdGrid.h
@@ -40,8 +40,8 @@ public:
     {
         dimM = m;
         dimN = n;
-        hx = (double)((domxh-domxl)/(n+1));
-        hy = (double)((domyh-domyl)/(m+1));
+        hx = (double)(2/(n+1));
+        hy = (double)(1/(m+1));
         
         std::cout << "..in FGrid Hx and Hy.." <<m << n << (domxh-domxl) << (domyh-domyl) << hx << "  " << hy << "\n";
         totdim = (dimM+LD) * (dimN+LD);
